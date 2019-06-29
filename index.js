9931fa1b5a47889b2437710847456adbb5d29630
Diego Ulises <diegoulisesmartinezaguilar@gmail.com>
2019-06-29 14:00:40 -0500

    Node.js Child Process: ejecucion de subprocesos con node.js

--- index.js
+++ index.js
@@ -1,33 +1,15 @@
-const readLine = require('readline')
+const cp = require('child_process')
 
-const rl = readLine.createInterface({
-    input: process.stdin,
-    output: process.stdout
-})
-
-const questions = ['Cuál es tu nombre? ', 'Cuáles son tus apellidos? ', 'Cuál es tu edad? ']
-
-const askQuestion = (rl, questions) => {
-    return new Promise((resolve, reject) => {
-        rl.question(questions, answer => {
-            resolve(answer)
-        })
-    })
-}
-
-const ask = questions=>{
-    return new Promise(async resolve =>{
-        let results =[]
-        for (let i = 0; i < questions.length; i++) {
-            const result = await askQuestion(rl, questions[i])
-            results.push(result)
+function exectCommand(command) {
+    cp.exec(command, (err, stdout, stderr)=>{
+        if(err){
+            console.log(`Error: ${err}`)
+            return
         }
-        rl.close()
-        resolve(results)
+        //if(err) throw err
+        if(stdout) console.log(`Standard out: \n${stdout}`)
+        if(stderr) console.log(`Standard err: \n${stderr}`)
     })
 }
-
-ask(questions)
-    .then(questions=>{
-        console.log(`Hola ${questions[0]} ${questions[1]} tu edad es la siguiente: ${questions[2]}`)
-    })
+//exectCommand('ls')
+exectCommand('node ./src/script --base=5')
