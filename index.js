a2d3556a2343c553675f98ab9122ba830c478e6f
Diego Ulises <diegoulisesmartinezaguilar@gmail.com>
2019-06-29 13:32:24 -0500

    Node.js ReadLIne: introduccion de datos por terminal

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
-const fs = require('fs')
-const opc = require('./src/fileops')
+const readLine = require('readline')
 
-fs.readFile('./resources/number.txt', 'utf8', (err, text) => {
-    if (err) throw err
-    const number = text.split('\n').map(n => Number(n))
-    //console.log(number)
-    incValues = opc.incrementValues(number)
-    fs.writeFile('./resources/numbernew.txt', incValues.join('\n'), (err, result) => {
-        if (err) throw err
-    })
+const rl = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout
 })
 
-fs.readFile('./resources/name.txt', 'utf8', (err, text) => {
-    if (err) throw err
-    const names = text.split('\n')
-    mapCall = opc.callNames(names)
-    fs.writeFile('./resources/namenew.txt', mapCall.join('\n'), (err, result) => {
-        if (err) throw err
+const questions = ['Cuál es tu nombre? ', 'Cuáles son tus apellidos? ', 'Cuál es tu edad? ']
+
+const askQuestion = (rl, questions) => {
+    return new Promise((resolve, reject) => {
+        rl.question(questions, answer => {
+            resolve(answer)
+        })
     })
-})
+}
+
+const ask = questions=>{
+    return new Promise(async resolve =>{
+        let results =[]
+        for (let i = 0; i < questions.length; i++) {
+            const result = await askQuestion(rl, questions[i])
+            results.push(result)
+        }
+        rl.close()
+        resolve(results)
+    })
+}
+
+ask(questions)
+    .then(questions=>{
+        console.log(`Hola ${questions[0]} ${questions[1]} tu edad es la siguiente: ${questions[2]}`)
+    })
