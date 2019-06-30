a1ce862140d2c5c1cbfe8360d6aae04bd7060c9c
Diego Ulises <diegoulisesmartinezaguilar@gmail.com>
2019-06-29 21:20:48 -0500

    Creacion de un servidor básico con HTTP
    Si una conexión de cliente emite un evento de ‘error’, se reenviará aquí. El "listener"de este evento es responsable de cerrar / destruir el socket subyacente. Por ejemplo, uno puede desear cerrar con más gracia el socket con una respuesta HTTP personalizada en lugar de cortar bruscamente la conexión.

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-const greet = require('./src/greet')
+const http = require('http')
+const fs = require('fs')
+const port = 3000
+const route = './resources/my_page.html'
 
-greet.emit('clap')
-greet.emit('shout', 'I LIKE')
-greet.emit('call', 'Diego Ulises', name => {
-    console.log(`Estamos llamando a ${name}`)
+const server = http.createServer((req,res) => {
+    fs.readFile(route,(err, data) => {
+        if(err) { console.error(err); return }
+        res.end(data)
+    })
 })
+
+console.log(`Iniciando servidor en el puerto ${port}`)
+server.listen(port)
