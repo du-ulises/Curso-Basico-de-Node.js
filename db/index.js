80e674d34adfb7885f4c3bb35892716d3e3211f6
Diego Ulises Martínez Aguilar <diegoulisesmartinezaguilar@gmail.com>
2019-06-30 23:49:52 -0500

    MongoDB: Conexión y escritura de datos con Node.js

--- db/index.js
+++ db/index.js
@@ -6,5 +6,8 @@ module.exports = {
     },
     getUserById: function(id) {
         return users.filter(user=>user._id===id);
+    },
+    getUserByAgeRange: function(lower = 0, higher = 99) {
+        return users.filter(user=>user.age >= lower && user.age <= higher);
     }
 }
--- index.js
+++ index.js
@@ -1,18 +1,28 @@
-//Borrar dependencies en package.json
-//npm install express nodemon --save
-
 const express = require("express");
 const app = express();
 const { PORT } = require("./config");
+const mongo = require("./db/connect");
 
 require("./routes/api")(app);
 require("./routes/views")(app);
 
-function init(){
+async function initDB(){
+    const db = await mongo.connect();
+    if (db) { initExpress(); }
+}
+
+function initExpress(){
     console.log("Iniciando instancia de Express...");
     app.listen(PORT, ()=>{
         console.log("El servidor Express esta activo.");
     });
+    process.on("SIGINT", closeApp);
+    process.on("SIGTERM", closeApp);
 }
 
-init();
+function closeApp(){
+    mongo.disconnect()
+        .then(()=>process.exit(0));
+}
+
+initDB();
